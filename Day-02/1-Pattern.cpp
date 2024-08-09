5709c9df8e25e739ff377c0e1c0ac1dcfedc902b
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-09 07:21:12 +0530

    Star Pattern

---
+++ Day-02/1-Pattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+for (int row = 0; row < 5; row++)
+{
+    for (int col = 0; col < 5; col++)
+    {
+        cout<<"* ";
+    }
+    cout<<endl;
+    
+}
+    
+
+return 0;
+
+}
