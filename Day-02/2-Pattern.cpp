a0ca324d8f327f0e3c0cd1915265ff7ebde6c2bf
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-09 07:22:39 +0530

    Number pattern

---
+++ Day-02/2-Pattern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    for (int row = 0; row < 5; row++)
+    {
+        for (int col = 0; col < 5; col++)
+        {
+            cout << row<<" ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
